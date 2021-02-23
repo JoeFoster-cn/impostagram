daf301a66e0c488a3ca7adfb6f3d84c1585ca771
Joe Foster <jfoster29492@gmail.com>
2021-02-23 15:20:34 +0000

    #33 Created Additional Story Examples
    Started styling

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -4,8 +4,8 @@ import './Stories.css'
 const Stories = () => {
     return (
         <div className="storiesContainer">
-            <p className="Stories">Stories</p>
-            <p className="Watch">Watch All</p>
+            <p id="stories">Stories</p>
+            <p id="watch">Watch All</p>
             <StoryExamples/>
         </div>
     )
@@ -14,12 +14,37 @@ const Stories = () => {
 const StoryExamples = () => {
     return (
         <div className="storyExamples">
-            <span className="example1">
-                <img src="/example1.jpg" alt="example1"/>
-                <h1> TEST NAME </h1>
+            <div className="images">
+                <img className="img1" src="../images/example1.jpg" alt="example1"/>
+                <img className="img2" src="../images/example2.jpg" alt="example2"/>
+                <img className="img3" src="../images/example3.jpg" alt="example3"/>
+                <img className="img4" src="../images/example4.jpg" alt="example4"/>
+            </div>
+            <div className="info">
+                <h1>Tommy Wiseau</h1>
+                <p id="timeAgo">8 MINUTES AGO</p>
+                <h1>Tommy Wiseau</h1>
+                <p id="timeAgo">8 MINUTES AGO</p>
+                <h1>Tommy Wiseau</h1>
+                <p id="timeAgo">8 MINUTES AGO</p>
+                <h1>Tommy Wiseau</h1>
+                <p id="timeAgo">8 MINUTES AGO</p>
+            </div>
+            {/* <div className="liveButtons">
+                <button id="live">LIVE</button>
+                <button id="live">LIVE</button>
+                <button id="live">LIVE</button>
+                <button id="live">LIVE</button>
+            </div> */}
+                {/* <h1> Tommy Wiseau </h1>
                 <p className="timeAgo">8 MINUTES AGO</p>
                 <button className="live">LIVE</button>
-            </span>
+            </span> */}
+                
+                {/* <h1> The Queen </h1>
+                <p className="timeAgo">6 MINUTES AGO</p>
+                <button className="live">LIVE</button>
+            </span> */}
         </div>
     ) 
 }
