87fa77f134d6a39661b3e38226083ddb79615722
Amelia <amelia.riding@Sofology.co.uk>
2021-02-23 10:27:40 +0000

    #22 Added Navbar container
    The navbar container contains Insta Logo, Search bar and icons.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
-function Navbar(){
-    return(
-        <div className="iconContainer">
+function Navbar() {
+    return (
+        <div className="navbarCont">
+            <div className="instaLogo">
 
+            </div>
+            <div className="searchBar">
+
+            </div>
+            <div className="iconContainer">
+
+            </div>
         </div>
     )
 }
