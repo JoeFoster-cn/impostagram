0d825d20e64a902a9a3fb8b6e9d193bdc877556c
Amelia <amelia.riding@Sofology.co.uk>
2021-02-23 16:25:12 +0000

    Testing Components Together

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import Navbar from './components/Navbar'
 import News from './components/News'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Stories from './components/Stories';
-import News from '.components/News';
-import Navbar from '.components/Navbar';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <Navbar/>
+      <Navbar />
+      <Stories />
       <News nickname="Zola" avatar="./images/ridge.jpg" image="./images/cake.jpg" caption="Made a cake!" likes="420" />
       <News nickname="Betty" avatar="./images/MrBeluga.jpg" image="./images/beluga.jpg" caption="Me and our kid" likes="69" />
-      <Stories/>
-=======
-      <Navbar />
-      <News />
->>>>>>> Stashed changes
     </div>
   );
 }
