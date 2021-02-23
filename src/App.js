7f928a5618f293cd0b178976e32ab91825ec3492
Joe Foster <jfoster29492@gmail.com>
2021-02-23 16:15:32 +0000

    #12 Added Cursor/additional styling
    Also updated App.js with component lines

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Stories from './components/Stories';
+import News from '.components/News';
+import Navbar from '.components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1> Test </h1>
+      <Navbar/>
+      <News nickname="Zola" avatar="./images/ridge.jpg" image="./images/cake.jpg" caption="Made a cake!" likes="420" />
+      <News nickname="Betty" avatar="./images/MrBeluga.jpg" image="./images/beluga.jpg" caption="Me and our kid" likes="69" />
+      <Stories/>
     </div>
   );
 }
