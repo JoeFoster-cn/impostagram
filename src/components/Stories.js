080d85ebe12e0d6225cc737a709ee1d66d94d5ae
Joe Foster <jfoster29492@gmail.com>
2021-02-23 12:14:20 +0000

    #31 Added Example Story images

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import './Stories.css'
 
+
+const Stories = () => {
+    return (
+        <div className="storiesContainer">
+            <p className="Stories">Stories</p>
+            <p classname="Watch">Watch All</p>
+            <storyExamples/>
+        </div>
+    )
+}
+
+const storyExamples = () => {
+    return (
+        <div className="storyExamples">
+            <p>test</p>
+            <img src="/example1.jpg" alt="example1"/>
+        </div>
+    ) 
+}
+
+export default Stories;
