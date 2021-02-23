b3084b7b9c422a8f62b5b05990857aea29a9bb2c
Joe Foster <jfoster29492@gmail.com>
2021-02-23 14:02:28 +0000

    #33 Added Story example 1 JSX

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Stories.css'
 
-
 const Stories = () => {
     return (
         <div className="storiesContainer">
             <p className="Stories">Stories</p>
-            <p classname="Watch">Watch All</p>
-            <storyExamples/>
+            <p className="Watch">Watch All</p>
+            <StoryExamples/>
         </div>
     )
-}
+} 
 
-const storyExamples = () => {
+const StoryExamples = () => {
     return (
         <div className="storyExamples">
-            <p>test</p>
-            <img src="/example1.jpg" alt="example1"/>
+            <span className="example1">
+                <img src="/example1.jpg" alt="example1"/>
+                <h1> TEST NAME </h1>
+                <p className="timeAgo">8 MINUTES AGO</p>
+                <button className="live">LIVE</button>
+            </span>
         </div>
     ) 
 }
