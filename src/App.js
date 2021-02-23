3501135adf4da37ebc1afae51bd9c58d9c60e623
Amelia Riding <ariding@csl.local>
2021-02-23 11:55:24 +0000

    Amended Navbar classNames and resized Insta logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Test </h1>
+      <Navbar />
     </div>
   );
 }
