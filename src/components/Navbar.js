b57510f70876e1888a393fb6156dae3d793105b0
Amelia <amelia.riding@Sofology.co.uk>
2021-02-23 10:54:30 +0000

    #26 Added Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,10 @@ function Navbar() {
 
             </div>
             <div className="iconContainer">
+            <i className="fas fa-home"></i>
+            <i class="far fa-paper-plane"></i>
+            <i class="far fa-compass"></i>
+            <i class="far fa-heart"></i>
 
             </div>
         </div>
