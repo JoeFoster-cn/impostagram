e35f26e11ed10fbfee3bf1fbde877372f3163de2
Amelia Riding <ariding@csl.local>
2021-02-23 11:43:44 +0000

    #30 added profile image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 function Navbar() {
     return (
         <div className="navbarCont">
-            <div className="instaLogo">
-                <img src = '../images/Instagram.png' />
-
+            <div>
+                <img className="instaLogo" src='../images/Instagram.png' />
             </div>
             <div className="searchBar">
-                <input type = "text"></input>
+                <input type="text"></input>
 
             </div>
             <div className="iconContainer">
-            <i className="fas fa-home"></i>
-            <i class="far fa-paper-plane"></i>
-            <i class="far fa-compass"></i>
-            <i class="far fa-heart"></i>
 
+                <i className="fas fa-home"></i>
+                <i class="far fa-paper-plane"></i>
+                <i class="far fa-compass"></i>
+                <i class="far fa-heart"></i>
+                <img src = "../images/Tommy.jpg" />
             </div>
         </div>
     )
