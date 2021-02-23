b05e7b32528c79b9add93f44ed99331e5e47874b
Amelia <amelia.riding@Sofology.co.uk>
2021-02-23 16:20:29 +0000

    Auto stash before merge of "main" and "origin/main"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar'
+import News from './components/News'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Stories from './components/Stories';
@@ -8,10 +9,15 @@ import Navbar from '.components/Navbar';
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <Navbar/>
       <News nickname="Zola" avatar="./images/ridge.jpg" image="./images/cake.jpg" caption="Made a cake!" likes="420" />
       <News nickname="Betty" avatar="./images/MrBeluga.jpg" image="./images/beluga.jpg" caption="Me and our kid" likes="69" />
       <Stories/>
+=======
+      <Navbar />
+      <News />
+>>>>>>> Stashed changes
     </div>
   );
 }
