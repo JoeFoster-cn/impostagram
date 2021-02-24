4a4ccd6e489ef34684ace902f01f220efb31e1db
Le-Ann Wright <leannwright21@gmail.com>
2021-02-24 11:28:14 +0000

    #34 Updated CSS to flexbox

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import './Suggestions.css';
 
-function Suggestions() {
+const Suggestions = () => {
     return (
-        <div className='wrapper'>
             <div className='heading'>
-                <h2>Suggestions For You</h2>
+                <h2 id='suggestionsTitle'>Suggestions For You</h2>  
+                <h2 id='seeall'>See All</h2>
+                <SuggestionExamples/>
+            </div>
+    )
+}
+
+const SuggestionExamples = () => {
+    return(
+                <div className='suggestionExamples'>
+               <div className='imgs'>
+                <img className='Beluga' src="/images/MrBeluga.jpg"></img>
+                <img className='Puppy'  src="/images/puppy.jpg"></img>
+                <img className='Kitty'  src="/images/concernedKitty.jpg" ></img>
             </div>
 
-        <div className='see-all'>
-        <h2>See All</h2>
-        </div>
+            <div className='suggestions'>
+                <h2>Mr. Beluga</h2>
+                <p id="post">New to Instagram</p>
+                <h2>Puppy</h2>
+                <p id="post">Followed by Mr. Beluga</p>
+                <h2>Concerned Kitty</h2>
+                <p id="post">Is worried</p>
 
-           
-                <img src="/images/MrBeluga.jpg"className='Beluga'></img>
-                <div className='suggestions-header-1'>
-                    <h2>Mr. Beluga</h2>
-                </div>
-                <div className='suggestions-body-1'>
-                    <p>New to Instagram</p>
-                </div>
-                <a href='#' className='follow-1'>Follow</a>
-       
-                <img src="/images/puppy.jpg"className='Puppy'></img>
-                <div className='suggestions-header-2'>
-                    <h2>Puppy</h2>
-                </div>
-                <div className='suggestions-body-2'>
-                    <p>Followed by Mr. Beluga</p>
-                </div>
-                <a href='#' className='follow-2'>Follow</a>
-           
-                <img src="/images/concernedKitty.jpg" className='Kitty'></img>
-                <div className='suggestions-header-3'>
-                    <h2>Concerned Kitty</h2>
-                </div>
-                <div className='suggestions-body-3'>
-                    <p>Is worried</p>
-                </div>
-                <a href='#' className='follow-3'>Follow</a>
+                {/* <div className='follow'>
+                    <p id="follow-1">Follow</p>
+                    <p id="follow-2">Follow</p>
+                    <p id="follow-3">Follow</p>
+                </div> */}
             </div>
-    
+         </div>
     )
 }
 
