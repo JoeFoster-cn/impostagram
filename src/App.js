99a3fa63b5e39bd09cc19c4dceea78ad39a35bcc
Ste Iddon <steiddon@gmail.com>
2021-02-25 15:43:47 +0000

    #63
    Added new story & made navbar stick to top when scrolling

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ function App() {
           <Footer />
         </div>
         <div className="newsfeed">
-          <VidNews nickname="Eric" avatar="./images/eagle.jpg" video="./videos/video1.mp4" caption="Flew up here for the views" likes="9000" />
-          <News nickname="Zola" avatar="./images/ridge.jpg" image="./images/cake.jpg" caption="Made a cake!" likes="420" />
+          <VidNews nickname="Ellie" avatar="./images/ellie.jpg" video="./videos/video2.mp4" caption="Out for a walk with the girls" likes="12" />
+          <VidNews nickname="Eric" avatar="./images/eagle.jpg" video="./videos/video1.mp4" caption="Flew up here for the views" likes="90" />
+          <News nickname="Zola" avatar="./images/ridge.jpg" image="./images/cake.jpg" caption="Made a cake!" likes="42" />
           <News nickname="Betty" avatar="./images/MrBeluga.jpg" image="./images/beluga.jpg" caption="Me and our kid" likes="69" />
         </div>
       </div>
